ccfb7f7b96fc6f8ee132890268ab36799e52b735
cfornetti <77817531+cfornetti@users.noreply.github.com>
2021-02-02 19:03:05 -0800

    Added the hours in JS

--- src/app.js
+++ src/app.js
@@ -5,3 +5,16 @@ function formatDate (timestamp){
     let day = days[date.getDay()];
     return `${day} ${formatHours(timestamp)}`;
 }
+
+function formatHours(timestamp){
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+     if (hours < 10) {
+        hours=`0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes=`0${minutes}`;
+    }
+    return`${hours}:${minutes}`;
+}
